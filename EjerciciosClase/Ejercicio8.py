89f5e60df69bbf15e02cc9a2236542ed3a0c1354
gustavo102699 <gus26bena@gmail.com>
2023-02-27 16:22:14 -0500

    update 8

---
+++ EjerciciosClase/Ejercicio8.py
@@ -0,0 +1,9 @@
+#8.Escribir un programa que almacene la cadena de caracteres contraseña en una variable, 
+# pregunte al usuario por la contraseña e imprima por pantalla si la contraseña introducida 
+# por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas.
+password = "PythoN2023"
+passwordnew = input("Ingresar contraseña: ")
+if password.lower() == passwordnew.lower():
+    print("La contraseña COINCIDE.")
+else:
+    print("La contraseña NO COINCIDE.")
